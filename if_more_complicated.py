78f6e219c97bd75c42de05288f216e45a7a34167
Bogdan Ratiu <bratiu-dn@drivenets.com>
2023-02-09 11:51:16 +0100

    class examples

---
+++ if_more_complicated.py
@@ -0,0 +1,15 @@
+try:
+    a = int(input("Give me a number pls "))
+    b = int(input("Give me another number pls "))
+except ValueError:
+    print("one of the numbers was invalid")
+else:
+    if a > b:
+        print(a, ">", b)
+    elif a < b:
+        print(a, "<", b)
+    else:
+        print(a, "=", b)
+
+
+
